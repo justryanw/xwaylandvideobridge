0fd276220ef92704d900e79f57dd85fd3e33ebb5
Ryan Walker <ryanjwalker2001@gmail.com>
2023-10-02 15:31:36 +0100

    move to flake

--- flake.nix
+++ flake.nix
@@ -10,7 +10,43 @@
     {
       packages = forAllSystems ({ pkgs }:
         {
-          default = pkgs.libsForQt5.callPackage ./xwaylandvideobridge.nix {};
+          default = pkgs.stdenv.mkDerivation (with pkgs; {
+            pname = "xwaylandvideobridge";
+            version = "unstable-2023-05-28";
+
+            src = fetchFromGitLab {
+              domain = "invent.kde.org";
+              owner = "system";
+              repo = "xwaylandvideobridge";
+              rev = "850ea0d965426e85746d5e2d4dd6f4c146403b7b";
+              hash = "sha256-5gRSkSdp9CGiVWjvIPKXPyEnI6hm1h2IeMunZymNa/M=";
+            };
+
+            nativeBuildInputs = [
+              cmake
+              extra-cmake-modules
+              pkg-config
+              libsForQt5.qt5.wrapQtAppsHook
+            ];
+
+            buildInputs = [
+              qt5.qtbase
+              qt5.qtquickcontrols2
+              qt5.qtx11extras
+              libsForQt5.kdelibs4support
+              (libsForQt5.kpipewire.overrideAttrs (oldAttrs: {
+                version = "unstable-2023-05-23";
+
+                src = fetchFromGitLab {
+                  domain = "invent.kde.org";
+                  owner = "plasma";
+                  repo = "kpipewire";
+                  rev = "600505677474a513be4ea8cdc8586f666be7626d";
+                  hash = "sha256-ME/9xOyRvvPDiYB1SkJLMk4vtarlIgYdlereBrYTcL4=";
+                };
+              }))
+            ];
+          });
         });
     };
 }
